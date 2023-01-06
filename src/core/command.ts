1c84f9c44dcd270ae6b623b075a5a57e15607082
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-01-06 10:40:23 +0530

    feat: add support for args and flags in create app command

--- src/core/command.ts
+++ src/core/command.ts
@@ -1,14 +1,12 @@
-import { Command } from "@contentstack/cli-command";
+import { Command } from '@contentstack/cli-command'
 
 export default abstract class AppsCommand extends Command {
-  setup(flags: any) {
+  setup(): void {
     if (!this.authToken) {
-      this.error("You need to login, first. See: auth:login --help", {
+      this.error('You need to login, first. See: auth:login --help', {
         exit: 2,
-        suggestions: [
-          "https://www.contentstack.com/docs/developers/cli/authentication/",
-        ],
-      });
+        ref: 'https://www.contentstack.com/docs/developers/cli/authentication/',
+      })
     }
   }
 }
