6c8411e2e220d043c117970132b439bb5487e587
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-01-06 10:40:23 +0530

    feat: add support for args and flags in create app command

--- src/commands/app/create.ts
+++ src/commands/app/create.ts
@@ -1,16 +1,52 @@
-import Command from "../../core/command";
+import { flags } from '@contentstack/cli-command'
+import Command from '../../core/command'
 
 export default class Create extends Command {
+  static description: string | undefined = 'create and register an app.'
+
+  static examples: string[] | undefined = [
+    '$ csdx app:create <app_name>',
+    '$ csdx app:create <app_name> --org "xxxxxxxxxxxxxxxxxxx" --app-type [stack/organization>]',
+    '$ csdx app:create <app_name> -o "xxxxxxxxxxxxxxxxxxx" -t [stack/organization>]',
+  ]
+
+  static args = [
+    {
+      name: 'appName',
+      description: 'Name of the app to be created',
+      required: true,
+    },
+  ]
+
+  static flags = {
+    org: flags.string({
+      char: 'o',
+      description: 'Organization UID',
+      required: true,
+    }),
+    'app-type': flags.string({
+      char: 't',
+      description: 'Type of App',
+      options: ['stack', 'organization'],
+      default: 'stack',
+      required: false,
+    }),
+  }
+
   async run(): Promise<any> {
     try {
-      const { flags } = this.parse(Create);
-      this.setup(flags);
+      const { flags, args } = this.parse(Create)
+      this.log('Flags and Args are', flags, args)
+      this.setup()
       const result: string = await new Promise((res) => {
-        setTimeout(() => res("Operation Performed successfully"), 3000);
-      });
-      this.log(result);
-    } catch (error) {
-      this.log("Something broke", error);
+        setTimeout(() => res('Operation Performed successfully'), 3000)
+      })
+      this.log(result)
+    } catch (error: any) {
+      this.error(error, {
+        exit: error.oclif.exit,
+        ref: error.suggestions,
+      })
     }
   }
 }
--- src/core/command.ts
+++ src/core/command.ts
@@ -1,4 +1,4 @@
-import { Command } from "@contentstack/cli-command";
+import { Command } from '@contentstack/cli-command'
 
 export default class AppsCommand extends Command {
   run(): PromiseLike<any> {
@@ -7,12 +7,10 @@ export default class AppsCommand extends Command {
 
   setup(flags: any) {
     if (!this.authToken) {
-      this.error("You need to login, first. See: auth:login --help", {
+      this.error('You need to login, first. See: auth:login --help', {
         exit: 2,
-        suggestions: [
-          "https://www.contentstack.com/docs/developers/cli/authentication/",
-        ],
-      });
+        ref: 'https://www.contentstack.com/docs/developers/cli/authentication/',
+      })
     }
   }
 }
