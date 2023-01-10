90ca3c85b89ea14189dd2bb6d06f766f1ee280c5
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-01-10 13:18:17 +0530

    feat: integrate app template and fs utils

---
+++ src/core/apps/build-project.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { createWriteStream } from 'node:fs'
+import ContentstackError from '../contentstack/error'
+
+export async function downloadProject(
+  projectUrl: string,
+  filePath: string
+): Promise<ContentstackError | undefined> {
+  try {
+    const writer = createWriteStream(filePath)
+    const response = await axios.get(projectUrl, {
+      method: 'GET',
+      responseType: 'stream',
+    })
+    response.data.pipe(writer)
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve)
+      writer.on('error', reject)
+    })
+  } catch (error: any) {
+    // Add an error code to identify failure
+    throw new ContentstackError(error.message, 401)
+  }
+}
+
+export function runCommand(path: string, command: string): void {}
