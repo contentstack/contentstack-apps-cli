44417814a44e6a1a11d84858aba5f9f20c7e23f9
Abhinav Gupta <abhinav.gupta@contentstack.com>
2023-07-02 16:43:19 +0530

    fix: handled errors

--- src/util/inquirer.ts
+++ src/util/inquirer.ts
@@ -32,12 +32,11 @@ async function getAppName(): Promise<string | boolean> {
  * @return {*}
  */
 async function getOrg(flags: FlagInput, options: CommonOptions) {
-  const { log } = options;
   const organizations = (await getOrganizations(options)) || [];
 
   if (!(flags.org && find(organizations, { uid: flags.org }))) {
     if (flags.org) {
-      log(messages.ORG_UID_NOT_FOUND, "error");
+      throw new Error(messages.ORG_UID_NOT_FOUND)
     }
 
     flags.org = await cliux
@@ -56,17 +55,19 @@ async function getOrg(flags: FlagInput, options: CommonOptions) {
 /**
  * @method 
  */
-async function getApp(flags: FlagInput, orgUid: string, options: CommonOptions) {
-  const { log } = options;
+async function getApp(flags: FlagInput, orgUid: string, options: CommonOptions) : Promise<Record<string, any> | undefined> {
   cliux.loader("Loading Apps");
-  const apps = (await getApps(orgUid, options)) || [];
+  const apps = (await getApps(flags, orgUid, options)) || [];
   cliux.loader("");
-  if (!(flags['app-uid'] && apps.find(app => app.uid === flags['app-uid']))) {
-    if (flags['app-uid']) {
-      log(messages.APP_UID_NOT_FOUND, "error");
+  if (apps.length === 0) {
+    throw new Error(messages.APPS_NOT_FOUND)
+  }
+  if (!(flags.app && apps.find(app => app.uid === flags.app))) {
+    if (flags.app) {
+      throw new Error(messages.APP_UID_NOT_FOUND)
     }
 
-    flags['app-uid'] = await cliux
+    flags.app = await cliux
       .inquire({
         type: "search-list",
         name: "App",
@@ -76,7 +77,7 @@ async function getApp(flags: FlagInput, orgUid: string, options: CommonOptions) 
       .then((name) => apps.find(app => app.name === name)?.uid)
   }
 
-  return apps.find(app => app.uid === flags['app-uid']);
+  return apps.find(app => app.uid === flags.app);
 }
 
 export { getOrg, getAppName, getApp };
