c5265c4ff741893d0064c0406367392adbf88a24
Antony <antonyagustineraj@gmail.com>
2023-07-04 11:47:01 +0530

    App create: boilerplate flow managed, dependency updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,11 @@
+import { join } from "path";
+
 const config = {
+  manifestPath: join(__dirname, "manifest.json"),
+  boilerplateName: "marketplace-app-boilerplate-main",
+  developerHubBaseUrl: "developerhub-api.contentstack.com",
   appBoilerplateGithubUrl:
     "https://github.com/contentstack/marketplace-app-boilerplate/archive/refs/heads/main.zip",
-  developerHubBaseUrl: "developerhub-api.contentstack.com",
-  boilerplateName: "marketplace-app-boilerplate-main",
 };
 
 export default config;
