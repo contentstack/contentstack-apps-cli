63523e7036a707cb37817e837533f98fabe49268
Antony <antonyagustineraj@gmail.com>
2023-07-05 17:24:00 +0530

    Error handling code added for app creation and cloning boilerplate

--- src/util/inquirer.ts
+++ src/util/inquirer.ts
@@ -1,7 +1,9 @@
 import find from "lodash/find";
+import { existsSync } from "fs";
+import { basename, dirname, join } from "path";
 import { FlagInput, cliux } from "@contentstack/cli-utilities";
 
-import messages, { errors } from "../messages";
+import messages, { $t, errors } from "../messages";
 import { CommonOptions, getOrganizations } from "./common-utils";
 
 /**
@@ -9,19 +11,51 @@ import { CommonOptions, getOrganizations } from "./common-utils";
  *
  * @return {*}  {(Promise<string | boolean>)}
  */
-async function getAppName(): Promise<string | boolean> {
+async function getAppName(defaultName: string = ""): Promise<string | boolean> {
   return cliux.inquire({
     type: "input",
     name: "appName",
-    message: messages.APP_NAME,
+    default: defaultName,
+    message: $t(messages.NAME, { target: "App" }),
     validate: (name) => {
       if (name.length < 3 || name.length > 20) {
-        return errors.INVALID_APP_NAME;
+        return $t(errors.INVALID_NAME, { min: "3", max: "20" });
       }
 
       return true;
     },
   });
+}
+
+/**
+ * @method getDirName
+ *
+ * @param {string} [defaultName=""]
+ * @return {*}  {(Promise<string | boolean>)}
+ */
+async function getDirName(path: string): Promise<string> {
+  const basePath = dirname(path);
+  const defaultName = basename(path);
+
+  return cliux
+    .inquire({
+      type: "input",
+      name: "dirName",
+      default: defaultName,
+      message: $t(messages.NAME, { target: "Directory" }),
+      validate: (name) => {
+        if (name.length < 3 || name.length > 30) {
+          return $t(errors.INVALID_NAME, { min: "3", max: "50" });
+        }
+
+        if (existsSync(join(basePath, name))) {
+          return $t(errors.INVALID_NAME, { min: "3", max: "50" });
+        }
+
+        return true;
+      },
+    })
+    .then((name) => join(basePath, name as string));
 }
 
 /**
@@ -53,4 +87,4 @@ async function getOrg(flags: FlagInput, options: CommonOptions) {
   return flags.org;
 }
 
-export { getOrg, getAppName };
+export { getOrg, getAppName, getDirName };
