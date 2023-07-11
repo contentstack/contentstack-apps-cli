f28403eae49f08207e24a2491342ccf88c04a830
Abhinav Gupta <abhinav.gupta@contentstack.com>
2023-07-11 16:41:07 +0530

    fix: added code review changes

--- src/commands/app/get.ts
+++ src/commands/app/get.ts
@@ -1,6 +1,7 @@
 import { BaseCommand } from "./base-command";
 import { getOrg, getApp, writeFile } from "../../util";
 import { flags } from "@contentstack/cli-utilities";
+import { commonMsg } from "../../messages";
 
 export default class Get extends BaseCommand<typeof Get> {
   static description = "Get app from the marketplace";
@@ -10,20 +11,13 @@ export default class Get extends BaseCommand<typeof Get> {
   ];
 
   static flags = {
-    'app': flags.string({
-      description: "App UID",
-    }),
-    'org': flags.string({
-      description: "Organization UID"
-    }),
-    'data-dir': flags.string({
-      description: "Path to the directory where the data will be saved",
-      default: "."
+    'app-uid': flags.string({
+      description: commonMsg.APP_UID,
     }),
     'app-type': flags.string({
       default: "stack",
       options: ["stack", "organization"],
-      description: "Type of app"
+      description: commonMsg.APP_TYPE_DESCRIPTION
     })
   };
 
@@ -31,7 +25,7 @@ export default class Get extends BaseCommand<typeof Get> {
     try {
       this.sharedConfig.org = await getOrg(this.flags, {managementSdk: this.managementSdk, log: this.log});
       let app = await getApp(this.flags, this.sharedConfig.org, {managementSdk: this.managementAppSdk, log: this.log})
-      await writeFile(this.flags['data-dir'], app, this.log)
+      await writeFile(this.flags['data-dir'], this.flags['yes'], app, this.log)
     } catch(error: any) {
       this.log(error.message, "error")
       this.exit()
