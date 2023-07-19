81e6649992cd84cf9231211abc96412e0b67521b
Antony <antonyagustineraj@gmail.com>
2023-07-19 11:18:56 +0530

    refactor: UI texts update

--- src/commands/app/get.ts
+++ src/commands/app/get.ts
@@ -4,32 +4,35 @@ import { flags } from "@contentstack/cli-utilities";
 import { commonMsg } from "../../messages";
 
 export default class Get extends BaseCommand<typeof Get> {
-  static description = "Get app from the marketplace";
+  static description = "Get details of an app in developer hub";
 
-  static examples = [
-    "$ <%= config.bin %> <%= command.id %>",
-  ];
+  static examples = ["$ <%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    'app-uid': flags.string({
+    "app-uid": flags.string({
       description: commonMsg.APP_UID,
     }),
-    'app-type': flags.string({
+    "app-type": flags.string({
       default: "stack",
       options: ["stack", "organization"],
-      description: commonMsg.APP_TYPE_DESCRIPTION
-    })
+      description: commonMsg.APP_TYPE_DESCRIPTION,
+    }),
   };
 
   async run(): Promise<void> {
     try {
-      this.sharedConfig.org = await getOrg(this.flags, {managementSdk: this.managementSdk, log: this.log});
-      let app = await getApp(this.flags, this.sharedConfig.org, {managementSdk: this.managementAppSdk, log: this.log})
-      await writeFile(this.flags['data-dir'], this.flags['yes'], app, this.log)
-    } catch(error: any) {
-      this.log(error.message, "error")
-      this.exit()
+      this.sharedConfig.org = await getOrg(this.flags, {
+        managementSdk: this.managementSdk,
+        log: this.log,
+      });
+      let app = await getApp(this.flags, this.sharedConfig.org, {
+        managementSdk: this.managementAppSdk,
+        log: this.log,
+      });
+      await writeFile(this.flags["data-dir"], this.flags["yes"], app, this.log);
+    } catch (error: any) {
+      this.log(error.message, "error");
+      this.exit();
     }
   }
-
 }
--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,19 +1,21 @@
 const errors = {
-  NOT_EMPTY: "{value} can't be empty",
-  INVALID_APP_ID: "App UID must be valid",
-  ORG_UID_NOT_FOUND: "Organization UID not found!",
-  BASE_URL_EMPTY: "Developer-hub URL can't be empty.",
-  INVALID_ORG_UID: "Please enter a valid organization uid.",
-  PATH_NOT_FOUND: "Provide path '{path}' is not valid/found.",
+  NOT_EMPTY: "{value} cannot be empty.",
+  INVALID_APP_ID: "Please enter a valid app UID.",
+  ORG_UID_NOT_FOUND:
+    "Organization UID not found. Please enter a valid organization UID.",
+  BASE_URL_EMPTY: "Developer Hub URL cannot be empty.",
+  INVALID_ORG_UID: "Please enter a valid organization UID.",
+  PATH_NOT_FOUND:
+    "Failed to locate the provided path '{path}'. Please enter a valid path.",
   INVALID_NAME:
     "Please enter a valid name that is {min} to {max} characters long.",
   FILE_GENERATION_FAILURE:
-    "Could not generate the file. Please try running the command again.",
-  APP_CREATION_FAILURE: "App could not be registered on Developer Hub",
+    "Failed to generate the file! Please try running the command again.",
+  APP_CREATION_FAILURE: "App could not be registered on Developer Hub.",
   APP_UID_NOT_MATCH:
-    "Provided App UID not matching with app manifest.json app UID",
+    "Provided app UID is not matching with the app manifest.json app UID",
   APP_CREATION_CONSTRAINT_FAILURE:
-    "App could not be registered. Please go through the constraints on app name and try running the command again.",
+    "App could not be registered. Please go through the constraints on the app name and try running the command again.",
   APP_INVALID_ORG:
     "App could not be registered. Please verify the inputs and try again.",
   DUPLICATE_APP_NAME:
@@ -24,48 +26,49 @@ const commonMsg = {
   CONFIG: "Path of the external config",
   MAX_RETRY_LIMIT: "Maximum retry limit reached.",
   PROVIDE_ORG_UID: "Provide the organization UID",
-  CURRENT_WORKING_DIR: "Current working directory",
-  SKIP_CONFIRMATION: "Use this flag to skip confirmation",
+  CURRENT_WORKING_DIR: "Current working directory.",
+  SKIP_CONFIRMATION: "Use this flag to skip the confirmation.",
   DEVELOPER_HUB_URL_PROMPT:
-    "Enter the developer-hub base URL for the {name} region - ",
+    "Enter the Developer Hub Base URL for the {name} region: ",
   APP_UID: "Provide the app UID",
   APP_TYPE_DESCRIPTION: "Type of App",
 };
 
 const appCreate = {
-  ORG_UID: "Org UID",
+  ORG_UID: "Organization UID",
   NAME: "{target} name",
-  UNZIP: "Unzipping the boilerplate",
+  UNZIP: "Unzipping the boilerplate...",
   APP_TYPE_DESCRIPTION: "Type of App",
   CHOOSE_ORG: "Choose an organization",
-  DIR_EXIST: "Directory name already exist",
-  ROLLBACK_BOILERPLATE: "Roll back boilerplate",
-  APP_UPDATE_FAILED: "Update app process failed",
-  INSTALL_DEPENDENCIES: "Installing dependencies",
+  DIR_EXIST: "Directory name already exists.",
+  ROLLBACK_BOILERPLATE: "Rolling back boilerplate...",
+  APP_UPDATE_FAILED: "App update process failed!",
+  INSTALL_DEPENDENCIES: "Installing dependencies...",
   NAME_DESCRIPTION: "Name of the app to be created",
-  APP_CREATION_SUCCESS: "App creation successful!",
-  CLONE_BOILERPLATE: "Fetching the app template from GitHub",
+  APP_CREATION_SUCCESS: "App created successfully!",
+  CLONE_BOILERPLATE: "Fetching the app template from GitHub...",
   CONFIRM_CLONE_BOILERPLATE:
-    "Would you like to fetching the app template from GitHub",
+    "Would you like to fetch the app template from GitHub?",
   REGISTER_THE_APP_ON_DEVELOPER_HUB:
-    "Registering the app with name {appName} on Developer Hub",
-  START_APP_COMMAND: "Start the app using following command: {command}",
+    "Registering the app with the name {appName} on the Developer Hub...",
+  START_APP_COMMAND: "Start the app using the following command: {command}",
 };
 
 const getApp = {
-  CHOOSE_APP: "Choose an App",
-  APP_UID_NOT_FOUND: "App UID not found",
+  CHOOSE_APP: "Choose an app",
+  APP_UID_NOT_FOUND: "App UID was not found!",
   FILE_WRITTEN_SUCCESS: "App data has been written to {file} successfully.",
-  APPS_NOT_FOUND: "No apps found",
-  FILE_ALREADY_EXISTS: "{file} already exists, would you like to save app to a new file? (Selecting No will over-write {file}) (y/n)"
-}
+  APPS_NOT_FOUND: "No apps found!",
+  FILE_ALREADY_EXISTS:
+    "{file} already exists. Would you like to save the app to a new file? (Selecting No will over-write {file}) (y/n)",
+};
 
 const appUpdate = {
   APP_UID: "Provide the app UID",
-  FILE_PATH: "Path to the {fileName} file",
-  APP_UPDATE_SUCCESS: "App update successful!",
+  FILE_PATH: "Path to the {fileName} file:",
+  APP_UPDATE_SUCCESS: "App updated successfully!",
   APP_VERSION_MISS_MATCH:
-    "App versions are not matching. Please download the latest file using the `csdx app:get` command and sync their file with this file",
+    "App versions mismatch! Please download the latest file using the `csdx app:get` command and sync the file with the latest downloaded file.",
 };
 
 const messages: typeof errors &
