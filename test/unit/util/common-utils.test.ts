fe3ebf1c27765de029c38a49661e0db1449054db
Antony <antonyagustineraj@gmail.com>
2023-07-24 20:31:40 +0530

    getOrganizations util function test case added

---
+++ test/unit/util/common-utils.test.ts
@@ -0,0 +1,60 @@
+import { fancy } from "fancy-test";
+import { expect } from "@oclif/test";
+import {
+  ContentstackClient,
+  configHandler,
+  managementSDKClient,
+} from "@contentstack/cli-utilities";
+
+import { getOrganizations } from "../../../src/util/common-utils";
+import * as mock from "../mock/common.mock.json";
+import { LogFn } from "../../../src/types";
+
+const region: { cma: string; name: string; cda: string } =
+  configHandler.get("region");
+
+describe("common utils", () => {
+  const log: LogFn = () => {};
+  let managementSdk: ContentstackClient;
+
+  before(async () => {
+    managementSdk = await managementSDKClient({
+      host: region.cma.replace("https://", ""),
+    });
+  });
+
+  describe("getOrganizations", () => {
+    describe("Get list of organizations", () => {
+      fancy
+        .nock(region.cma, (api) =>
+          api
+            .get(
+              "/v3/organizations?limit=100&asc=name&include_count=true&skip=0"
+            )
+            .reply(200, { organizations: mock.organizations })
+        )
+        .it("Returns list of org", async () => {
+          const [org1, org2] = await getOrganizations({ log, managementSdk });
+          expect(org1.uid).to.equal(mock.organizations[0].uid);
+          expect(org2.uid).to.equal(mock.organizations[1].uid);
+        });
+    });
+
+    describe("Get list of organizations failure case", async () => {
+      fancy
+        .nock(region.cma, (api) =>
+          api
+            .get(
+              "/v3/organizations?limit=100&asc=name&include_count=true&skip=0"
+            )
+            .reply(400)
+        )
+        .do(async () => await getOrganizations({ log, managementSdk }))
+        .catch((err) => {
+          const { status }: { status: number } = JSON.parse(err.message);
+          expect(status).to.equal(400);
+        })
+        .it("API fails with status code 400");
+    });
+  });
+});
