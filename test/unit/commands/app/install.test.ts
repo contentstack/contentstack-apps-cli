5754660f258b43c5a7b8ae66f69e216880bc34aa
Abhinav Gupta <abhinav.gupta@contentstack.com>
2023-08-03 10:54:53 +0530

    feat: added one more test case for install

--- test/unit/commands/app/install.test.ts
+++ test/unit/commands/app/install.test.ts
@@ -162,7 +162,53 @@ describe("app:install", () => {
         })
         .it("should install a stack app")
     })
+    describe("App is already installed", () => {
+        test
+        .stdout({ print: process.env.PRINT === "true" || false })
+        .stub(ux.action, "stop", () => {})
+        .stub(ux.action, "start", () => {})
+        .stub(cliux, "inquire", async (...args: any) => {
+            const [prompt]: any = args;
+            const cases = {
+                App: mock.apps[1].name,
+                Organization: mock.organizations[0].name,
+            };
+
+            return (cases as Record<string, any>)[prompt.name];
+        })
+        .nock(region.cma, (api) =>
+        api
+            .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
+            .reply(200, { organizations: mock.organizations })
+        )
+        .nock(`https://${developerHubBaseUrl}`, (api) =>
+        api
+            .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
+            .reply(200, {
+            data: mock.apps,
+            })
+        )
+        .nock(`https://${developerHubBaseUrl}`, (api) =>
+        api
+            .post(`/manifests/${mock.apps[1].uid}/install`, {
+                "target_type": mock.apps[1].target_type,
+                "target_uid": mock.organizations[0].uid
+            })
+            .replyWithError({
+                "status": 400,
+                "message": "Installation for app is already done",
+                "error": "Internal Server Error"
+            })
+        )
+        .command([
+            "app:install"
+        ])
+        .exit(1)
+        .do(({stdout}) => {
+            expect(stdout).to.contain("Installation for app is already done")
+        })
+        .it("should fail with an error that app is already installed")
+    })
     // describe("Invalid app provided")
-    // describe("App is already installed")
     // describe("Stack API Key is provided, but the app selected is an organization app")
 })
