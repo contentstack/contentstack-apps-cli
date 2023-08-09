745707c24f070ea9c84a9c61d0964fbfff789a12
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2023-08-09 12:23:18 +0100

    fix: fixed issue with the management sdk params

--- src/interfaces/uninstall-app.ts
+++ src/interfaces/uninstall-app.ts
@@ -1,7 +1,7 @@
-import { FlagInput } from "@contentstack/cli-utilities";
+import { ContentstackClient, FlagInput } from "@contentstack/cli-utilities";
 import { CommonOptions } from "../util";
 import { AppTarget } from "@contentstack/management/types/app/index"
 
 export interface UninstallApp {
-  run(flags: FlagInput, org: string, options: CommonOptions, appType: AppTarget): Promise<void>;
+  run(flags: FlagInput, org: string, managementSdk: ContentstackClient, options: CommonOptions, appType: AppTarget): Promise<void>;
 }
--- src/strategies/uninstall-selected.ts
+++ src/strategies/uninstall-selected.ts
@@ -1,21 +1,21 @@
-import { FlagInput } from "@contentstack/cli-utilities";
+import { ContentstackClient, FlagInput } from "@contentstack/cli-utilities";
 import { UninstallApp } from "../interfaces/uninstall-app";
 import { CommonOptions, getInstallation, uninstallApp } from "../util";
 import { AppTarget } from "@contentstack/management/types/app/index"
 
 export class UninstallSelected implements UninstallApp {
-  public async run(flags: FlagInput, org: string, options: CommonOptions, appType: AppTarget): Promise<void> {
+  public async run(flags: FlagInput, org: string, managementSdk: ContentstackClient, options: CommonOptions, appType: AppTarget): Promise<void> {
     // select installation uid to uninstall
-    const installationUids = await this.getInstallations(flags, org, options, appType)
+    const installationUids = await this.getInstallations(flags, org, managementSdk, options, appType)
     for (const installationUid of installationUids) {
       await uninstallApp(flags, org, options, installationUid)
     }
   }
 
-  public async getInstallations(flags: FlagInput, org: string, options: CommonOptions, appType: AppTarget): Promise<string[]> {
+  public async getInstallations(flags: FlagInput, org: string, managementSdk: ContentstackClient, options: CommonOptions, appType: AppTarget): Promise<string[]> {
     let installationUids: any = flags['installation-uid'];
     if (!installationUids) {
-      installationUids = await getInstallation(flags, org, options.managementSdk, appType, options)
+      installationUids = await getInstallation(flags, org, managementSdk, appType, options)
     }
     return installationUids.split(',')
   }
