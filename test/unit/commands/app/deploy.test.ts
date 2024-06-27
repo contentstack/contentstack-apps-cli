a59500521635d85fd0e19daa538b6da9d8eb370f
harshithad0703 <harshitha.d@contentstack.com>
2024-06-27 15:31:49 +0530

    test: added 1 more test case using flags in command

--- test/unit/commands/app/deploy.test.ts
+++ test/unit/commands/app/deploy.test.ts
@@ -5,13 +5,53 @@ import * as mock from "../../mock/common.mock.json";
 
 import messages, { $t } from "../../../../src/messages";
 import { getDeveloperHubUrl } from "../../../../src/util/inquirer";
+// import { join } from "path";
 
 const region: { cma: string; cda: string; name: string } =
   configHandler.get("region");
 const developerHubBaseUrl = getDeveloperHubUrl();
 
 describe("app:deploy", () => {
-  describe("Deploy an app with custom hosting", () => {
+    describe("Deploy an app with custom hosting", () => {
+      test
+        .stdout({ print: process.env.PRINT === "true" || false })
+        .stub(ux.action, "stop", () => {})
+        .stub(ux.action, "start", () => {})
+        .stub(cliux, "inquire", async (...args: any) => {
+          const [prompt]: any = args;
+          const cases = {
+            App: mock.apps[1].name,
+            Organization: mock.organizations[0].name,
+            "hosting types": "Custom Hosting",
+            appUrl: "https://example.com",
+          };
+          return (cases as Record<string, any>)[prompt.name];
+        })
+        .nock(region.cma, (api) =>
+          api
+            .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
+            .reply(200, { organizations: mock.organizations })
+        )
+        .nock(`https://${developerHubBaseUrl}`, (api) =>
+          api
+            .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
+            .reply(200, {
+              data: mock.apps2,
+            })
+        )
+        .nock(`https://${developerHubBaseUrl}`, (api) =>
+          api.put(`/manifests/${mock.apps2[1].uid}`).reply(200, mock.deploy_custom_host)
+        )
+        .command(["app:deploy"])
+        .do(({ stdout }) => {
+          expect(stdout).to.contain(
+            $t(messages.APP_DEPLOYED, { app: mock.apps[1].name })
+          );
+        })
+        .it("should deploy the app with custom hosting");
+    });
+
+  describe("Deploy an app with custom hosting using flags in command", () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
       .stub(ux.action, "stop", () => {})
@@ -33,21 +73,31 @@ describe("app:deploy", () => {
       )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
         api
-          .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
-          .reply(200, {
-            data: mock.apps2,
-          })
-      )
+            .get(`/manifests/${mock.apps2[1].uid}`)
+            .reply(200, {
+            data: mock.apps2[1],
+            })
+        )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
         api.put(`/manifests/${mock.apps2[1].uid}`).reply(200, mock.deploy_custom_host)
       )
-      .command(["app:deploy"])
+      .command([
+        "app:deploy",
+        "--org",
+        mock.organizations[0].uid,
+        "--app-uid",
+        mock.apps[1].uid,
+        "--hosting-type",
+        "Custom Hosting",
+        "--app-url",
+        "https://example.com",
+      ])
       .do(({ stdout }) => {
         expect(stdout).to.contain(
           $t(messages.APP_DEPLOYED, { app: mock.apps[1].name })
         );
       })
-      .it("should deploy the app with custom hosting");
+      .it("should deploy the app with custom hosting using flags in command");
   });
 
   describe("Deploy an app with Hosting with Launch with existing project", () => {
