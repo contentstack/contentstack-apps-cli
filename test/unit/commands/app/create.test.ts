1f838fa171a093c9b7c1767ecc3d049cadc2f582
harshithad0703 <harshitha.d@contentstack.com>
2024-08-02 14:38:21 +0530

    updated create test cases

--- test/unit/commands/app/create.test.ts
+++ test/unit/commands/app/create.test.ts
@@ -12,7 +12,7 @@ import config from "../../../../src/config";
 import messages from "../../../../src/messages";
 import * as mock from "../../mock/common.mock.json";
 import manifestData from "../../../../src/config/manifest.json";
-import manifestData2 from "../../../unit/config/manifest.json";
+import orgManifestData from "../../../unit/config/org_manifest.json";
 import { getDeveloperHubUrl } from "../../../../src/util/inquirer";
 
 const { origin, pathname } = new URL(config.appBoilerplateGithubUrl);
@@ -26,7 +26,6 @@ describe("app:create", () => {
     axios.defaults.adapter = "http";
   });
 
-  //working
   describe("Creating a stack app using a boilerplate flow", () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
@@ -79,7 +78,6 @@ describe("app:create", () => {
       .it("should create a stack level app");
   });
 
-  // //working
   describe("Creating a organization app using a boilerplate flow", () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
@@ -115,14 +113,14 @@ describe("app:create", () => {
       .nock(`https://${developerHubBaseUrl}`, (api) =>
         api
           .post("/manifests", {
-            ...manifestData2,
+            ...orgManifestData,
             name: "test-app",
             target_type: "organization",
           })
-          
+
           .reply(200, {
             data: {
-              ...manifestData2,
+              ...orgManifestData,
               name: "test-app",
               version: 1,
             },
@@ -143,14 +141,14 @@ describe("app:create", () => {
       .it("should create a organization level app");
   });
 
-  describe("Creating a app without boilerplate", () => {
+  describe("Creating an app without boilerplate", () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
       .stub(ux.action, "stop", () => {})
       .stub(ux.action, "start", () => {})
       .stub(fs, "renameSync", () => new PassThrough())
       .stub(fs, "writeFileSync", () => new PassThrough())
-      .stub(cliux, "inquire", async (...args: any) => {
+      .stub(cliux, "inquire", async (...args) => {
         const [prompt]: any = args;
         const cases = {
           appName: "test-app",
@@ -167,12 +165,20 @@ describe("app:create", () => {
       )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
         api
-          .post("/manifests", { ...manifestData, name: "test-app" })
+          .post("/manifests", (body) => {
+            return body.name === "test-app" && body.target_type === "stack";
+          })
           .reply(200, {
             data: { ...manifestData, name: "test-app", version: 1 },
           })
       )
-      .command(["app:create", "--data-dir", process.cwd()])
+      .command([
+        "app:create",
+        "--name",
+        "test-app",
+        "--data-dir",
+        process.cwd(),
+      ])
       .do(({ stdout }) =>
         expect(stdout).to.contain(messages.APP_CREATION_SUCCESS)
       )
@@ -225,15 +231,6 @@ describe("app:create", () => {
       .stdout({ print: process.env.PRINT === "true" || false })
       .stub(ux.action, "stop", () => {})
       .stub(ux.action, "start", () => {})
-      .stub(shelljs, "cd", () => {})
-      .stub(shelljs, "exec", (...args) => {
-        const [, , callback]: any = args;
-        callback(0);
-      })
-      .stub(fs, "renameSync", () => new PassThrough())
-      .stub(fs, "writeFileSync", () => new PassThrough())
-      .stub(fs, "createWriteStream", () => new PassThrough())
-      .stub(tmp, "fileSync", () => ({ name: zipPath }))
       .stub(cliux, "inquire", async (...args: any) => {
         const [prompt]: any = args;
         const cases = {
@@ -241,12 +238,8 @@ describe("app:create", () => {
           cloneBoilerplate: false,
           Organization: "test org 1",
         };
-
         return (cases as Record<string, any>)[prompt.name];
       })
-      .nock(origin, (api) =>
-        api.get(pathname).reply(200, { data: "test-data" })
-      )
       .nock(region.cma, (api) =>
         api
           .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
@@ -254,33 +247,19 @@ describe("app:create", () => {
       )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
         api
-          .post("/manifests", {
-            ...manifestData2,
-            name: "test-app",
-            target_type: "organization",
+          .post("/manifests", (body) => {
+            return body.name === "test-app" && body.target_type === "stack";
           })
-          
-          .reply(200, {
-            data: {
-              ...manifestData2,
-              name: "test-app",
-              version: 1,
-            },
+          .reply(400, {
+            errorMessage: "App creation failed due to constraints.",
           })
       )
-      .command([
-        "app:create",
-        "--name",
-        "test-app",
-        "--data-dir",
-        process.cwd(),
-        "--app-type",
-        "organization",
-      ])
-      .do(({ stdout }) =>
-        expect(stdout).to.contain(messages.APP_CREATION_CONSTRAINT_FAILURE)
-      )
-      .it("App creation should fail!");
+      .command(["app:create", "--data-dir", process.cwd()])
+      .exit(1)
+      .do(({ stdout }) => {
+        expect(stdout).to.contain(messages.APP_CREATION_CONSTRAINT_FAILURE);
+      })
+      .it("App creation should fail and rollback");
   });
 
   describe("Pass external config using '--config' flag", () => {
@@ -304,25 +283,29 @@ describe("app:create", () => {
           .reply(200, { organizations: mock.organizations })
       )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api.post("/manifests", { ...manifestData, name: "test-app" }).reply(400)
+        api
+          .post("/manifests", (body) => {
+            return body.name === "test-app" && body.target_type === "stack";
+          })
+          .reply(400, {
+            errorMessage: "App creation failed due to constraints.",
+          })
       )
       .command([
         "app:create",
-        "--name",
-        "test-app",
         "--data-dir",
         process.cwd(),
         "--config",
         resolve(process.cwd(), "test", "unit", "mock", "config.json"),
       ])
       .exit(1)
-      .do(({ stdout }) =>
-        expect(stdout).to.contain(messages.APP_CREATION_CONSTRAINT_FAILURE)
-      )
+      .do(({ stdout }) => {
+        // Ensure the error message is what you expect
+        expect(stdout).to.contain(messages.APP_CREATION_CONSTRAINT_FAILURE);
+      })
       .it("App creation should fail!");
   });
 
-  //working
   describe("Dependency installation failure", () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
@@ -337,7 +320,7 @@ describe("app:create", () => {
       .stub(fs, "writeFileSync", () => new PassThrough())
       .stub(fs, "createWriteStream", () => new PassThrough())
       .stub(tmp, "fileSync", () => ({ name: zipPath }))
-      .stub(cliux, "inquire", async (...args: any) => {
+      .stub(cliux, "inquire", async (...args) => {
         const [prompt]: any = args;
         const cases = {
           appName: "test-app",
@@ -356,9 +339,13 @@ describe("app:create", () => {
           .reply(200, { organizations: mock.organizations })
       )
       .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api.post("/manifests", { ...manifestData }).reply(200, {
-          data: { ...manifestData, name: "test-app", version: 1 },
-        })
+        api
+          .post("/manifests", (body) => {
+            return body.name === "test-app" && body.target_type === "stack";
+          })
+          .reply(200, {
+            data: { ...manifestData, name: "test-app", version: 1 },
+          })
       )
       .command([
         "app:create",
