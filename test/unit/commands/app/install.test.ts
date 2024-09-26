ad595191e2e6b1c52b4eb84248e29b83fd103ce6
harshitha.d <harshitha.d@contentstack.com>
2024-09-26 15:40:14 +0530

    updated install test suits

--- test/unit/commands/app/install.test.ts
+++ test/unit/commands/app/install.test.ts
@@ -1,223 +1,163 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import nock from "nock";
+import sinon from "sinon";
 import { runCommand } from "@oclif/test";
-import { cliux, configHandler, ux } from "@contentstack/cli-utilities";
-import sinon from 'sinon';
+import { cliux, ux, configHandler } from "@contentstack/cli-utilities";
+import messages from "../../../../src/messages";
 import * as mock from "../../mock/common.mock.json";
-import messages, { $t } from "../../../../src/messages";
 import { getDeveloperHubUrl } from "../../../../src/util/inquirer";
-import fancy from 'fancy-test';
+import axios from "axios";
 
-const region: { cma: string; cda: string; name: string } = configHandler.get("region");
+const region = configHandler.get("region");
 const developerHubBaseUrl = getDeveloperHubUrl();
 
 describe("app:install", () => {
+  let sandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    axios.defaults.adapter = "http";
+
+    sandbox.stub(ux.action, "stop").callsFake(() => {});
+    sandbox.stub(ux.action, "start").callsFake(() => {});
+
+    nock(region.cma)
+      .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
+      .reply(200, { organizations: mock.organizations });
+
+    nock(`https://${developerHubBaseUrl}`)
+      .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
+      .reply(200, { data: mock.apps });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+    nock.cleanAll();
+  });
+
   describe("Install an app on organization", () => {
     beforeEach(() => {
-      sinon.stub(ux.action, "stop").callsFake(() => {});
-      sinon.stub(ux.action, "start").callsFake(() => {});
-      sinon.stub(cliux, "inquire").callsFake(async (prompt: any) => {
-        const cases = {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
           App: mock.apps[1].name,
           Organization: mock.organizations[0].name,
         };
-        return (cases as Record<string, any>)[prompt.name];
+        return cases[prompt.name];
       });
-      sinon.stub(cliux, "confirm").resolves(true);
-      sinon.stub(configHandler, "get").withArgs("region").returns(region);
+      nock(`https://${developerHubBaseUrl}`)
+        .post(`/manifests/${mock.apps[1].uid}/install`, {
+          target_type: mock.apps[1].target_type,
+          target_uid: mock.organizations[0].uid,
+        })
+        .reply(200, { data: mock.apps });
     });
 
-    afterEach(() => {
-      sinon.restore();
+    it("should install an organization app", async () => {
+      const { stdout } = await runCommand(["app:install"]);
+      expect(stdout).to.contain(
+        messages.INSTALLING_APP_NOTICE.replace("{app}", mock.apps[1].name)
+          .replace("{type}", mock.apps[1].target_type)
+          .replace("{target}", mock.organizations[0].uid)
+      );
     });
-
-    fancy
-      .nock(region.cma, (api) =>
-        api
-          .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-          .reply(200, { organizations: mock.organizations })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
-          .reply(200, { data: mock.apps })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .post(`/manifests/${mock.apps[1].uid}/install`, {
-            target_type: mock.apps[1].target_type,
-            target_uid: mock.organizations[0].uid,
-          })
-          .reply(200, { data: mock.apps })
-      )
-      .it("should install an organization app", async () => {
-        const { stdout } = await runCommand(["app:install"]);
-        expect(stdout).to.contain(
-          $t(messages.INSTALLING_APP_NOTICE, {
-            app: mock.apps[1].name,
-            type: mock.apps[1].target_type,
-            target: mock.organizations[0].uid,
-          })
-        );
-      });
   });
 
   describe("Install an app on a stack", () => {
     beforeEach(() => {
-      sinon.stub(ux.action, "stop").callsFake(() => {});
-      sinon.stub(ux.action, "start").callsFake(() => {});
-      sinon.stub(cliux, "inquire").callsFake(async (prompt: any) => {
-        const cases = {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
           App: mock.apps[0].name,
           Organization: mock.organizations[0].name,
           Stack: mock.stacks[0].name,
         };
-        return (cases as Record<string, any>)[prompt.name];
+        return cases[prompt.name];
       });
-      sinon.stub(configHandler, "get").withArgs("region").returns(region);
+      nock(region.cma)
+        .get(
+          `/v3/organizations/${mock.organizations[0].uid}/stacks?limit=100&asc=name&include_count=true&skip=0`
+        )
+        .reply(200, { stacks: mock.stacks });
+
+      nock(`https://${developerHubBaseUrl}`)
+        .post(`/manifests/${mock.apps[0].uid}/install`, {
+          target_type: mock.apps[0].target_type,
+          target_uid: mock.stacks[0].api_key,
+        })
+        .reply(200, { data: mock.apps });
     });
 
-    afterEach(() => {
-      sinon.restore();
+    it("should install a stack app", async () => {
+      const { stdout } = await runCommand(["app:install"]);
+      expect(stdout).to.contain(
+        messages.APP_INSTALLED_SUCCESSFULLY.replace(
+          "{app}",
+          mock.apps[0].name
+        ).replace("{target}", mock.stacks[0].name)
+      );
     });
-
-    fancy
-      .nock(region.cma, (api) =>
-        api
-          .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-          .reply(200, { organizations: mock.organizations })
-      )
-      .nock(region.cma, (api) =>
-        api
-          .get(`/v3/organizations/${mock.organizations[0].uid}/stacks?limit=100&asc=name&include_count=true&skip=0`)
-          .reply(200, { stacks: mock.stacks })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
-          .reply(200, { data: mock.apps })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .post(`/manifests/${mock.apps[0].uid}/install`, {
-            target_type: mock.apps[0].target_type,
-            target_uid: mock.stacks[0].api_key,
-          })
-          .reply(200, { data: mock.apps })
-      )
-      .it("should install a stack app", async () => {
-        const { stdout } = await runCommand(["app:install"]);
-        expect(stdout).to.contain(
-          $t(messages.APP_INSTALLED_SUCCESSFULLY, {
-            app: mock.apps[0].name,
-            target: mock.stacks[0].name,
-          })
-        );
-      });
   });
 
   describe("Stack API Key and App ID provided through flags", () => {
     beforeEach(() => {
-      sinon.stub(ux.action, "stop").callsFake(() => {});
-      sinon.stub(ux.action, "start").callsFake(() => {});
-      sinon.stub(cliux, "inquire").callsFake(async (prompt: any) => {
-        const cases = {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
           Organization: mock.organizations[0].name,
         };
-        return (cases as Record<string, any>)[prompt.name];
+        return cases[prompt.name];
       });
-      sinon.stub(configHandler, "get").withArgs("region").returns(region);
+      nock(region.cma).get(`/v3/stacks`).reply(200, { stack: mock.stacks[0] });
+      nock(`https://${developerHubBaseUrl}`)
+        .get(`/manifests/${mock.apps[0].uid}`)
+        .reply(200, {
+          data: mock.apps[0],
+        });
+      nock(`https://${developerHubBaseUrl}`)
+        .post(`/manifests/${mock.apps[0].uid}/install`, {
+          target_type: mock.apps[0].target_type,
+          target_uid: mock.stacks[0].api_key,
+        })
+        .reply(200, { data: mock.apps });
     });
 
-    afterEach(() => {
-      sinon.restore();
+    it("should install a stack app", async () => {
+      const { stdout } = await runCommand([
+        "app:install",
+        "--stack-api-key",
+        mock.stacks[0].api_key,
+        "--app-uid",
+        mock.apps[0].uid,
+      ]);
+      expect(stdout).to.contain(
+        messages.APP_INSTALLED_SUCCESSFULLY.replace(
+          "{app}",
+          mock.apps[0].name
+        ).replace("{target}", mock.stacks[0].name)
+      );
     });
-
-    fancy
-      .nock(region.cma, (api) =>
-        api
-          .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-          .reply(200, { organizations: mock.organizations })
-      )
-      .nock(region.cma, (api) =>
-        api
-          .get(`/v3/stacks`)
-          .reply(200, { stack: mock.stacks[0] })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .get(`/manifests/${mock.apps[0].uid}`)
-          .reply(200, { data: mock.apps[0] })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .post(`/manifests/${mock.apps[0].uid}/install`, {
-            target_type: mock.apps[0].target_type,
-            target_uid: mock.stacks[0].api_key,
-          })
-          .reply(200, { data: mock.apps })
-      )
-      .it("should install a stack app with flags", async () => {
-        const { stdout } = await runCommand([
-          "app:install",
-          "--stack-api-key",
-          mock.stacks[0].api_key,
-          "--app-uid",
-          mock.apps[0].uid,
-        ]);
-
-        expect(stdout).to.contain(
-          $t(messages.APP_INSTALLED_SUCCESSFULLY, {
-            app: mock.apps[0].name,
-            target: mock.stacks[0].name,
-          })
-        );
-      });
   });
 
   describe("App is already installed", () => {
     beforeEach(() => {
-      sinon.stub(ux.action, "stop").callsFake(() => {});
-      sinon.stub(ux.action, "start").callsFake(() => {});
-      sinon.stub(cliux, "inquire").callsFake(async (prompt: any) => {
-        const cases = {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
           App: mock.apps[1].name,
           Organization: mock.organizations[0].name,
         };
-        return (cases as Record<string, any>)[prompt.name];
+        return cases[prompt.name];
       });
-      sinon.stub(configHandler, "get").withArgs("region").returns(region);
+      nock(`https://${developerHubBaseUrl}`)
+        .post(`/manifests/${mock.apps[1].uid}/install`, {
+          target_type: mock.apps[1].target_type,
+          target_uid: mock.organizations[0].uid,
+        })
+        .reply(400, {
+          message: "Installation for app is already done",
+        });
     });
 
-    afterEach(() => {
-      sinon.restore();
+    it("should fail with an error that app is already installed", async () => {
+      const { stdout } = await runCommand(["app:install"]);
+      expect(stdout).to.contain("Installation for app is already done");
     });
-
-    fancy
-      .nock(region.cma, (api) =>
-        api
-          .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-          .reply(200, { organizations: mock.organizations })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
-          .reply(200, { data: mock.apps })
-      )
-      .nock(`https://${developerHubBaseUrl}`, (api) =>
-        api
-          .post(`/manifests/${mock.apps[1].uid}/install`, {
-            target_type: mock.apps[1].target_type,
-            target_uid: mock.organizations[0].uid,
-          })
-          .replyWithError({
-            status: 400,
-            message: "Installation for app is already done",
-            error: "Internal Server Error",
-          })
-      )
-      .it("should fail with an error that app is already installed", async () => {
-        const { stdout } = await runCommand(["app:install"]);
-        expect(stdout).to.contain("Installation for app is already done");
-      });
   });
 });
