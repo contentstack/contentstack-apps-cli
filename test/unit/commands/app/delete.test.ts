affbff16c900b6e279f39b7f2fd7c30601d76fbd
harshitha.d <harshitha.d@contentstack.com>
2024-09-27 15:45:20 +0530

    refactor create and delete suits

--- test/unit/commands/app/delete.test.ts
+++ test/unit/commands/app/delete.test.ts
@@ -12,18 +12,27 @@ const region: { cma: string; name: string; cda: string } =
 const developerHubBaseUrl = getDeveloperHubUrl();
 
 describe("app:delete", () => {
-  describe("app:delete with --org and --app-uid flags", () => {
-    it("should print an error saying that app is already installed", async () => {
-      nock(region.cma)
-        .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-        .reply(200, { organizations: mock.organizations });
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    nock(region.cma)
+      .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
+      .reply(200, { organizations: mock.organizations });
+  });
+  afterEach(() => {
+    sandbox.restore();
+    nock.cleanAll();
+  });
 
+  describe("app:delete with --org and --app-uid flags", () => {
+    beforeEach(() => {
       nock(`https://${developerHubBaseUrl}`)
         .get("/manifests/app-uid-1/installations")
         .reply(200, {
           data: mock.installations,
         });
-
+    });
+    it("should print an error saying that app is already installed", async () => {
       const { stdout } = await runCommand([
         "app:delete",
         "--org",
@@ -31,28 +40,20 @@ describe("app:delete", () => {
         "--app-uid",
         mock.apps[0].uid,
       ]);
-
       expect(stdout).to.contain(messages.APP_IS_INSTALLED);
-      nock.cleanAll();
     });
   });
 
   describe("app:delete using inquirer prompts", () => {
-    it("should delete the app", async () => {
-      const inquireStub = sinon
-        .stub(cliux, "inquire")
-        .callsFake(async (prompt: any) => {
-          const cases: Record<string, any> = {
-            Organization: "test org 1",
-            App: "App 1",
-            confirmation: true,
-          };
-          return Promise.resolve(cases[prompt.name]);
-        });
-
-      nock(region.cma)
-        .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-        .reply(200, { organizations: mock.organizations });
+    beforeEach(() => {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
+          Organization: "test org 1",
+          App: "App 1",
+          confirmation: true,
+        };
+        return Promise.resolve(cases[prompt.name]);
+      });
 
       nock(`https://${developerHubBaseUrl}`)
         .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
@@ -65,38 +66,25 @@ describe("app:delete", () => {
       nock(`https://${developerHubBaseUrl}`)
         .delete("/manifests/app-uid-1")
         .reply(200, { data: {} });
-
+    });
+    it("should delete the app", async () => {
       const { stdout } = await runCommand(["app:delete"]);
-
       expect(stdout).to.contain(
         $t(messages.APP_DELETED_SUCCESSFULLY, { app: mock.apps[0].name })
       );
-
-      inquireStub.restore();
-      nock.cleanAll();
     });
   });
 
   describe("app:delete error handling", () => {
-    afterEach(() => {
-      sinon.restore();
-      nock.cleanAll();
-    });
-    it("should handle error with status 409 and statusText 'Conflict'", async () => {
-      const inquireStub = sinon
-        .stub(cliux, "inquire")
-        .callsFake(async (prompt: any) => {
-          const cases: Record<string, any> = {
-            Organization: "test org 1",
-            App: "App 1",
-            confirmation: true,
-          };
-          return Promise.resolve(cases[prompt.name]);
-        });
-
-      nock(region.cma)
-        .get("/v3/organizations?limit=100&asc=name&include_count=true&skip=0")
-        .reply(200, { organizations: mock.organizations });
+    beforeEach(() => {
+      sandbox.stub(cliux, "inquire").callsFake(async (prompt: any) => {
+        const cases: Record<string, any> = {
+          Organization: "test org 1",
+          App: "App 1",
+          confirmation: true,
+        };
+        return Promise.resolve(cases[prompt.name]);
+      });
       nock(`https://${developerHubBaseUrl}`)
         .get("/manifests?limit=50&asc=name&include_count=true&skip=0")
         .reply(200, { data: mock.apps });
@@ -108,10 +96,10 @@ describe("app:delete", () => {
           error: "Bad Request",
           statusText: "Conflict",
         });
+    });
+    it("should handle error with status 409 and statusText 'Conflict'", async () => {
       const { stderr } = await runCommand(["app:delete"]);
       expect(stderr).to.contain(messages.CONTACT_SUPPORT);
-      inquireStub.restore();
-      nock.cleanAll();
     });
   });
 });
